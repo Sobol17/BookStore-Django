16a31e3150dee7c8199aa10e1aeb59613ee246fa
sobol17 <sobolinskii@mail.ru>
2025-10-19 17:50:59 +0800

    Оптимизировал views и деструктуризировал шаблоны, настроил поиск

--- bookstore/main/views.py
+++ bookstore/main/views.py
@@ -33,19 +33,25 @@ class CatalogView(TemplateView):
         'author': lambda queryset, value: queryset.filter(authors__icontains=value),
     }
 
+    @staticmethod
+    def _is_truthy(value):
+        if value is None:
+            return False
+        return str(value).lower() in {'1', 'true', 'on', 'yes'}
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         category_slug = kwargs.get('category_slug')
         categories = Category.objects.all()
-        product = Product.objects.all().order_by('-created_at')
+        products = Product.objects.all().order_by('-created_at')
         current_category = None
         if category_slug:
             current_category = get_object_or_404(Category, slug=category_slug)
-            product = product.filter(category=current_category)
+            products = products.filter(category=current_category)
         query = self.request.GET.get('q')
         if query:
-            product = product.filter(
+            products = products.filter(
                 Q(name__icontains=query) | Q(authors__icontains=query) | Q(publisher__icontains=query)
             )
         filter_params = {}
@@ -60,15 +66,20 @@ class CatalogView(TemplateView):
         context.update({
             'categories': categories,
             'products': products,
-            'current_category': category_slug,
+            'current_category': current_category.slug if current_category else None,
+            'current_category_label': current_category.name if current_category else None,
             'filter_params': filter_params,
             'genres': Genre.objects.all(),
             'search_query': query or '',
         })
-        if self.request.headers.GET.get('show_search') == True:
+        if self._is_truthy(self.request.GET.get('show_search')):
             context['show_search'] = True
-        elif self.request.headers.GET.get('reset_search') == True:
+        if self._is_truthy(self.request.GET.get('reset_search')):
             context['reset_search'] = True
+        if self._is_truthy(self.request.GET.get('show_filter')):
+            context['show_filter'] = True
+        if self._is_truthy(self.request.GET.get('reset_filter')):
+            context['reset_filter'] = True
         return context
     
     def get(self, request, *args, **kwargs):
@@ -78,8 +89,11 @@ class CatalogView(TemplateView):
                 return TemplateResponse(request, 'main/search_input.html', context)
             elif context.get('reset_search'):
                 return TemplateResponse(request, 'main/search_button.html', {})
-            template = 'main/filter_modal.html' if request.headers.get('show_filter') == True else 'main/catalog.html'
-            return TemplateResponse(request, template, context)
+            if context.get('show_filter'):
+                return TemplateResponse(request, 'main/filter_modal.html', context)
+            if context.get('reset_filter'):
+                return HttpResponse('')
+            return TemplateResponse(request, 'main/catalog.html', context)
         return TemplateResponse(request, self.template_name, context)
 
 
@@ -97,7 +111,12 @@ class ProductDetailView(DetailView):
         context['related_products'] = Product.objects.filter(
             category=product.category
         ).exclude(id=product.id)[:4]
-        context['current_category'] = product.category.slug
+        if product.category:
+            context['current_category'] = product.category.slug
+            context['current_category_label'] = product.category.name
+        else:
+            context['current_category'] = None
+            context['current_category_label'] = None
         return context
     
     def get(self, request, *args, **kwargs):
@@ -105,4 +124,4 @@ class ProductDetailView(DetailView):
         context = self.get_context_data(**kwargs)
         if request.headers.get('HX-Request'):
             return TemplateResponse(request, 'main/product_detail.html', context)
-        raise TemplateResponse(request, self.template_name, context)
+        return TemplateResponse(request, self.template_name, context)
