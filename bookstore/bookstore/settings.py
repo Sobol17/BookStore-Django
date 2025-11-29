4869fa83315233ca9652134281bc73ee5e2eb2a9
sobol17 <sobolinskii@mail.ru>
2025-11-29 17:13:50 +0800

    update settings

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -190,4 +190,11 @@ AUTH_USER_MODEL = 'users.CustomUser'
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'no-reply@bookstore.local')
 EMAIL_LINK_MAX_AGE = int(os.getenv('EMAIL_LINK_MAX_AGE', '1800'))
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '25'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
     
