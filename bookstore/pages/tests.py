70bcec8f65d2f505ff40f14b00e502d4da2bea7e
sobol17 <sobolinskii@mail.ru>
2026-02-25 21:30:40 +0800

    add new urls and tests

--- bookstore/pages/tests.py
+++ bookstore/pages/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Page
+
+
+class CustomPagesTests(TestCase):
+    def test_about_page_available(self):
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/about_page.html')
+        self.assertEqual(response.context['seo_title'], 'О магазине Dombb')
+
+    def test_contacts_page_available(self):
+        response = self.client.get('/contacts')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/custom_page.html')
+        self.assertEqual(response.context['seo_title'], 'Контакты Dombb')
+
+    def test_dynamic_page_still_works(self):
+        page = Page.objects.create(
+            title='Тестовая страница',
+            slug='test-page',
+            is_published=True,
+            content='<p>Контент страницы</p>',
+        )
+        response = self.client.get(f'/{page.slug}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/page_detail.html')
--- bookstore/pages/urls.py
+++ bookstore/pages/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PageDetailView
+from .views import AboutPageView, ContactsPageView, PageDetailView
 
 
 app_name = 'pages'
 
 urlpatterns = [
+	path('about', AboutPageView.as_view(), name='about'),
+	path('about/', AboutPageView.as_view()),
+	path('contacts', ContactsPageView.as_view(), name='contacts'),
+	path('contacts/', ContactsPageView.as_view()),
 	path('<uslug:slug>/', PageDetailView.as_view(), name='page'),
 ]
